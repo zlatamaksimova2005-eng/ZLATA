983f46eda055bf2d1bddcb2918f83823e20793ae
zlatamaksimova2005-eng <zlata.maksimova2005@gmail.com>
2026-01-18 19:29:49 +0300

    ЛБ4

---
+++ ЛБ4.ЗД2.py
@@ -0,0 +1,25 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME, 'r', encoding='utf-8') as csv_file:
+
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+
+        data = list(csv_reader)
+
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
