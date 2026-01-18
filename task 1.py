087f2212b54eb665e93eae66ff04dc95fcc1ad76
zlatamaksimova2005-eng <zlata.maksimova2005@gmail.com>
2026-01-18 23:26:34 +0300

    ПР1

---
+++ task 1.py
@@ -0,0 +1,16 @@
+PI = 3.1415  # константа PI
+radius = 5  # радиус круга
+side = 5  # сторона квадрата
+
+# Расчет периметра и площади круга
+circle_area = PI * radius ** 2
+circle_circumference = 2 * PI * radius
+
+# Расчет периметра и площади квадрата
+square_perimeter = 4 * side
+square_area = side ** 2
+
+print("Площадь круга:", round(circle_area, 2))
+print("Длина окружности:", round(circle_circumference, 2))
+print("Периметр квадрата:", square_perimeter)
+print("Площадь квадрата:", square_area)
---
+++ task 6.py
@@ -0,0 +1,13 @@
+results = [10, 8, 9, 7, 6, 9, 10, 8, 9, 10]
+
+total = sum(results)
+count = len(results)
+
+average = total / count
+
+min_score = min(results)
+max_score = max(results)
+
+print("Среднее арифметическое результатов выстрелов:", average)
+print("Наименьшее количество очков за выстрел:", min_score)
+print("Наибольшее количество очков за выстрел:", max_score)
