14e34b3c4f7905589c099999f259fe921b935ece
zlatamaksimova2005-eng <zlata.maksimova2005@gmail.com>
2026-01-18 19:17:53 +0300

    ЛБ1

---
+++ ЛБ1.ЗД3.py
@@ -0,0 +1,9 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+middle_index = len(list_players) // 2
+
+first_team = list_players[:middle_index]
+second_team = list_players[middle_index:]
+
+print(first_team)
+print(second_team)
